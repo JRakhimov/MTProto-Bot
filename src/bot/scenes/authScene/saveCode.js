aa53c1ce82ac6225000efcedd92d5ba967f23bef
RJ <rakhimov.5556@gmail.com>
2018-07-27 10:55:14 +0500

    feat: saving account information about logged in user

--- src/bot/scenes/authScene/saveCode.js
+++ src/bot/scenes/authScene/saveCode.js
@@ -9,9 +9,22 @@ saveCode.on("text", async ctx => {
 
   const user = await ctx.MTProto.authSignIn(ctx.message.text);
 
-  ctx.helper.mainKeyboard(ctx, `Signed in as ${user.username}`);
+  const userData = {
+    first_name: user.first_name || "",
+    access_hash: user.access_hash,
+    last_name: user.last_name || "",
+    username: user.username || "",
+    photo: user.photo || "",
+    phone: user.phone,
+    id: user.id
+  }
 
-  ctx.database.ref(`sessions/${ctx.chat.id}/${ctx.chat.id}/__scenes`).remove();
+  ctx.helper.mainKeyboard(ctx, `Signed in as ${userData.username}`);
+
+  ctx.database.ref(`sessions/${ctx.chat.id}/${ctx.chat.id}/__scenes`).set(null);
+
+  ctx.database.ref("/MTProtoAccount/").set(userData);
+
   return ctx.scene.leave();
 });
 
