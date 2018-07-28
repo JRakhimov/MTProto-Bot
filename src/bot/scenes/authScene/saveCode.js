4d7d343382c1722c6084eafeeef7d569be0b7adb
RJ <rakhimov.5556@gmail.com>
2018-07-28 11:23:29 +0500

    fix: validating user code and phone

--- src/bot/scenes/authScene/saveCode.js
+++ src/bot/scenes/authScene/saveCode.js
@@ -5,27 +5,34 @@ const Composer = require("telegraf/composer");
 const saveCode = new Composer();
 
 saveCode.on("text", async ctx => {
-  ctx.reply("Signing in...");
+  const code = ctx.message.text;
 
-  const { user } = await ctx.MTProto.authSignIn(ctx.message.text.split("_")[1]);
+  if (code.match(/_\d\d\d\d\d/) != null) {
+    ctx.reply("Signing in...");
 
-  const userData = {
-    first_name: user.first_name || "",
-    access_hash: user.access_hash,
-    last_name: user.last_name || "",
-    username: user.username || "",
-    photo: user.photo || "",
-    phone: user.phone,
-    id: user.id
-  };
+    const { user } = await ctx.MTProto.authSignIn(code.split("_")[1]);
 
-  ctx.Helper.mainKeyboard(ctx, `Signed in as ${userData.username}`);
+    const userData = {
+      first_name: user.first_name || "",
+      access_hash: user.access_hash,
+      last_name: user.last_name || "",
+      username: user.username || "",
+      photo: user.photo || "",
+      phone: user.phone,
+      id: user.id
+    };
 
-  ctx.Database.ref(`sessions/${ctx.chat.id}/${ctx.chat.id}/__scenes`).set(null);
+    ctx.Helper.mainKeyboard(
+      ctx,
+      `Signed in as ${userData.username || user.first_name}`
+    );
 
-  ctx.Database.ref("/MTProtoAccount/Me").set(userData);
+    ctx.Database.ref("/MTProtoAccount/Me").set(userData);
 
-  return ctx.scene.leave();
+    return ctx.scene.leave();
+  } else {
+    ctx.reply("Make sure the code is sent correctly👨‍✈️");
+  }
 });
 
 module.exports = saveCode;
--- src/bot/scenes/authScene/saveNumber.js
+++ src/bot/scenes/authScene/saveNumber.js
@@ -4,12 +4,25 @@ const Composer = require("telegraf/composer");
 
 const saveNumber = new Composer();
 
-saveNumber.on("text", ctx => {
-  ctx.MTProto.authSendCode(ctx.message.text);
+saveNumber.on("text", async ctx => {
+  if (
+    ctx.message.entities != null &&
+    ctx.message.entities[0].type === "phone_number"
+  ) {
+    await ctx.MTProto.authSendCode(ctx.message.text);
 
-  ctx.reply("Code:");
+    // Telegram automatically invalidates your login code
+    // if it is sent to any chat on telegram before the login,
+    // this problem can be avoided simply by appending or pretending some chars to the code.
 
-  return ctx.wizard.next();
+    ctx.reply(
+      'Code sent✨\n\nSend code with adding char "_" before the code. E.g.: _49326'
+    );
+
+    return ctx.wizard.next();
+  } else {
+    ctx.reply("Make sure the number is sent correctly👨‍✈️");
+  }
 });
 
 module.exports = saveNumber;
