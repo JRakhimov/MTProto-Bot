03e1857bbc235d9dd317588aa1b6f4513cbbd3ba
RJ <rakhimov.5556@gmail.com>
2018-08-09 16:54:23 +0500

    feat: ban on raising one's own karma

--- src/bot/routers/karmaHandler.js
+++ src/bot/routers/karmaHandler.js
@@ -40,22 +40,27 @@ const karma = new Router(({ update }) => {
 
 karma.on("username", ctx => {
   const CURRENT_MONTH = moment().format("MMMM");
+  const fromID = ctx.chat.id < 0 ? ctx.message.from.id : ctx.chat.id;
 
   ctx.MTProto.contactsResolveUsername(ctx.state.mentionedUser)
     .then(async user => {
       const chatID = user.peer.user_id;
 
-      const userData = ctx.Database.ref(
-        `${botConfig.karmaPath}/${CURRENT_MONTH}/${chatID}`
-      );
+      if (fromID != chatID) {
+        const userData = ctx.Database.ref(
+          `${botConfig.karmaPath}/${CURRENT_MONTH}/${chatID}`
+        );
 
-      let userKarma = (await userData.once("value")).val();
+        let userKarma = (await userData.once("value")).val();
 
-      userKarma =
-        ctx.state.command === "++" ? (userKarma += 1) : (userKarma -= 1);
-      userData.set(userKarma);
+        userKarma =
+          ctx.state.command === "++" ? (userKarma += 1) : (userKarma -= 1);
+        userData.set(userKarma);
 
-      ctx.reply(`@${ctx.state.mentionedUser}: ${userKarma}`);
+        ctx.reply(`@${ctx.state.mentionedUser}: ${userKarma}`);
+      } else {
+        ctx.reply("You can't raise your own karma!");
+      }
     })
     .catch(err => {
       ctx.reply(`User with username: ${ctx.state.mentionedUser} not found!`);
