a0789c1a337a1c4783023762fa3f4261ee16a0f2
RJ <rakhimov.5556@gmail.com>
2018-08-15 11:35:52 +0500

    feat(general): directReply and contact forward to admin

--- src/bot/composers/general.js
+++ src/bot/composers/general.js
@@ -10,21 +10,38 @@ general.start((ctx, next) => {
   const isAdmin = ctx.Helper.isAdmin(chatID);
 
   if (ctx.chat.type === "private" && !isAdmin) {
-    ctx.reply("Hi!");
-  } else if (isAdmin) {
+    ctx.Helper.shareContact(
+      ctx,
+      "Hi! Please click the button below to share your contact information:"
+    );
+  } else if (ctx.chat.type === "private" && isAdmin) {
     next(ctx);
   } else {
     ctx.reply("This command is only available in private chat!");
   }
 });
 
+general.on("contact", ctx => {
+  ctx.telegram.forwardMessage(
+    botConfig.admins[0],
+    ctx.chat.id,
+    ctx.message.message_id
+  );
+  ctx.reply("Done!");
+});
+
 general.command("/karmame", async ctx => {
   const CURRENT_MONTH = moment().format("MMMM");
   const chatID = ctx.chat.id < 0 ? ctx.message.from.id : ctx.chat.id;
+
   const myKarma = (await ctx.Database.ref(
     `${botConfig.karmaPath}/${CURRENT_MONTH}/${chatID}`
   ).once("value")).val();
-  ctx.reply(`Your karma for ${CURRENT_MONTH} is ${myKarma || 0}`);
+
+  ctx.Helper.directReply(
+    ctx,
+    `Your karma for ${CURRENT_MONTH} is ${myKarma || 0}`
+  );
 });
 
 general.command("/karmatop", async ctx => {
@@ -32,7 +49,10 @@ general.command("/karmatop", async ctx => {
 
   const allKarma = await ctx.Helper.getAllKarma(ctx.Database);
 
-  ctx.reply(`Here is our top for ${CURRENT_MONTH}:\n\n` + allKarma.join(""));
+  ctx.Helper.directReply(
+    ctx,
+    `Here is our top for ${CURRENT_MONTH}:\n\n` + allKarma.join("")
+  );
 });
 
 module.exports = general;
